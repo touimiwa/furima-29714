a3138ca91b0a6da659ed6fccb2ed30a0d895fce2
touimiwa <toy10078toy@icloud.com>
2020-09-13 17:24:38 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ApplicationRecord
   validates :nickname, presence: true
+  validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ--]+\z/} do
+    validates :kanalast_name
+    validates :kanafirst_name
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
