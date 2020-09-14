c5f001b6ca2264503ce8c1a90d77890029a659e0
touimiwa <toy10078toy@icloud.com>
2020-09-14 15:41:47 +0900

    単体テストコード実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {"abe"}
+    email                 {"kkk@gmail.com"}
+    password              {"toy1007"}
+    password_confirmation {password}
+    last_name             {"阿部"}
+    first_name            {"太郎"}
+    kanalast_name         {"アベ"}
+    kanafirst_name        {"タロウ"}
+    birthday              {"2000-11-11"}
   end
 end
