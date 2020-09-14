9970555a517907a7ac34aa4c7ce10e5ab76f5e43
touimiwa <toy10078toy@icloud.com>
2020-09-14 20:02:33 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,20 @@
 class User < ApplicationRecord
-  validates :nickname, presence: true
-  validates :birthday, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-    validates :last_name
-    validates :first_name
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+    
+      with_options  format: { with: /\A[ァ-ヶ--]+\z/} do
+        validates :kanalast_name
+        validates :kanafirst_name
+      end
   end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶ--]+\z/} do
-    validates :kanalast_name
-    validates :kanafirst_name
-  end
+    
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
