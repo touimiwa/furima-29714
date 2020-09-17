a0d4ec42507675835581f481b8a9de98c0972f0d
touimiwa <toy10078toy@icloud.com>
2020-09-17 12:51:06 +0900

    非同期通信

---
+++ app/javascript/price.js
@@ -0,0 +1,38 @@
+window.addEventListener('load', price);
+
+function price(){
+
+   const priceInput = document.getElementById("item-price");
+   const add_tax = document.getElementById("add-tax-price");
+   const profit = document.getElementById("profit");
+
+     priceInput.addEventListener('keyup', () => {
+         const value = priceInput.value; 
+         
+       if (value >= 300 && value <= 9999999){
+         let fee = Math.floor(value * 0.1)
+         let gains = value - fee;
+         add_tax.textContent = fee;
+         profit.textContent = gains;
+     } else {
+       let fee = '-';
+       let gains = '-';
+       add_tax.textContent = fee;
+       profit.textContent = gains;
+     }
+   });
+  
+  }
+ 
+
+
+
+
+
+
+
+
+
+
+// "add-tax-price" = 販売手数料
+// "profit" = 販売利益
