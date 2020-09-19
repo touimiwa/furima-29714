90dac4495242c2f8785495a030e94aac7bee8221
touimiwa <toy10078toy@icloud.com>
2020-09-20 08:11:58 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
