a47e278d477cee6be5953527d94e0fc3aa4b8151
touimiwa <toy10078toy@icloud.com>
2020-09-28 17:41:07 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
 
   def index
